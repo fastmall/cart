1f660a6e6cf7ee9af5d1ecd9d4d840799134d17a
Zhu Rengen <zhurungen@wecash.net>
2021-11-21 21:09:37 +0800

    add import

--- dubbo/dubbo.go
+++ dubbo/dubbo.go
@@ -6,20 +6,21 @@ import (
 	_ "dubbo.apache.org/dubbo-go/v3/imports"
 	_ "github.com/dubbogo/triple/pkg/triple"
 	"github.com/fastmall/cart/service"
+	customerApi "github.com/fastmall/customer/api"
 	goodsApi "github.com/fastmall/goods/api"
+	orderApi "github.com/fastmall/order/api"
 )
 
 var GoodsService = &goodsApi.GoodsServiceClientImpl{}
+var CustomerService = &customerApi.CustomerServiceClientImpl{}
+var OrderService = &orderApi.OrderServiceClientImpl{}
 
 func StartDubbo() {
 	config.SetConsumerService(GoodsService)
+	config.SetConsumerService(CustomerService)
+	config.SetConsumerService(OrderService)
 	config.SetProviderService(&service.CartService{})
 	err := config.Load(config.WithPath("conf/dubbo.yaml"))
-	if err != nil {
-		logger.Fatal(err)
-		return
-	}
-	err = config.Load(config.WithPath("conf/dubbo.yaml"))
 	if err != nil {
 		logger.Fatal(err)
 		return
--- go.mod
+++ go.mod
@@ -8,7 +8,9 @@ require (
 	dubbo.apache.org/dubbo-go/v3 v3.0.0-rc4-1
 	github.com/dubbogo/triple v1.1.5
 	github.com/fastmall/cart/api v0.0.0-00010101000000-000000000000
+	github.com/fastmall/customer/api v0.0.2
 	github.com/fastmall/goods/api v0.0.0-20211121110358-668d3d5fef60
+	github.com/fastmall/order/api v0.0.0-20211121125026-a7ef77c4009c
 )
 
 require (
